9837bb41f0bc86c837f145906f5b6edc21348d8d
Alireza Askari <alireza2023asgari@gmail.com>
2023-11-16 09:43:31 +0330

    I changed server.js

--- server.js
+++ server.js
@@ -1,71 +1,36 @@
-import fs from "node:fs/promises";
+//server-dev.js
+import fs from "fs";
 import express from "express";
+import { createServer } from "vite";
 
-// Constants
-const isProduction = process.env.NODE_ENV === "production";
-const port = process.env.PORT || 3300;
-const base = process.env.BASE || "/";
-
-// Cached production assets
-const templateHtml = isProduction
-  ? await fs.readFile("./dist/client/index.html", "utf-8")
-  : "";
-const ssrManifest = isProduction
-  ? await fs.readFile("./dist/client/ssr-manifest.json", "utf-8")
-  : undefined;
-
-// Create http server
 const app = express();
 
-// Add Vite or respective production middlewares
-let vite;
-if (!isProduction) {
-  const { createServer } = await import("vite");
-  vite = await createServer({
-    server: { middlewareMode: true },
-    appType: "custom",
-    base,
-  });
-  app.use(vite.middlewares);
-} else {
-  const compression = (await import("compression")).default;
-  const sirv = (await import("sirv")).default;
-  app.use(compression());
-  app.use(base, sirv("./dist/client", { extensions: [] }));
-}
+const vite = await createServer({
+  server: {
+    middlewareMode: true,
+  },
+  appType: "custom",
+});
 
-// Serve HTML
+app.use(vite.middlewares);
+
 app.use("*", async (req, res) => {
+  const url = req.originalUrl;
+
   try {
-    const url = req.originalUrl.replace(base, "");
+    const template = await vite.transformIndexHtml(
+      url,
+      fs.readFileSync("index.html", "utf-8")
+    );
+    const { render } = await vite.ssrLoadModule("/src/entry-server.jsx");
 
-    let template;
-    let render;
-    if (!isProduction) {
-      // Always read fresh template in development
-      template = await fs.readFile("./index.html", "utf-8");
-      template = await vite.transformIndexHtml(url, template);
-      render = (await vite.ssrLoadModule("/src/entry-server.jsx")).render;
-    } else {
-      template = templateHtml;
-      render = (await import("./dist/server/entry-server.js")).render;
-    }
-
-    const rendered = await render(url, ssrManifest);
-
-    const html = template
-      .replace(`<!--app-head-->`, rendered.head ?? "")
-      .replace(`<!--app-html-->`, rendered.html ?? "");
-
+    const html = template.replace(`<!--outlet-->`, render);
     res.status(200).set({ "Content-Type": "text/html" }).end(html);
-  } catch (e) {
-    vite?.ssrFixStacktrace(e);
-    console.log(e.stack);
-    res.status(500).end(e.stack);
+  } catch (error) {
+    res.status(500).end(error);
   }
 });
 
-// Start http server
-app.listen(port, () => {
-  console.log(`Server started at http://localhost:${port}`);
+app.listen(4173, () => {
+  console.log("http://localhost:4173.");
 });
